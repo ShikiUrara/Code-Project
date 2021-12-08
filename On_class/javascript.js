656bc57b06632b142c56ee06b9731de60aa121e5
FreeTime010 <time0009000@gmail.com>
2021-12-08 17:57:22 +0900

    changes

--- On_class/javascript.js
+++ On_class/javascript.js
@@ -1,7 +1,6 @@
 // strict
 "use strict"
 
-
 // Bai 1 Bo sung chuc nang tim kiem du lieu
 
 function searching() {
@@ -19,7 +18,6 @@ function enterCheck(event) {
 
 function searchResult() {
     var url = new URL(window.location);
-    var wd =  url.searchParams.get("words");
     // if(wd == null) 
     //     document.getElementById("out").innerHTML = "Không tìm thấy từ khóa đã nhập"
     // else
@@ -124,6 +122,75 @@ function cart() {
     window.location.href = "donhang.html"
 }
 
-function showCart() {
+function showCart() {}
 
+
+
+// Lab 4 - 1
+var d=new Date();
+var ads="Khách hàng có ngày sinh trong tháng "+ ( d.getMonth() + 1 ) +" sẽ được tặng 2 phần sữa chua dâu cho đơn hàng đầu tiên trong tháng.";
+$(document).ready(function() {
+    $("footer").append( "<div id='adscontainer'><span id='adstext'><h2>"+ads+"</h2></span></div>");
+    var w = ($(window).width() - $("main").width())/2;
+    alert(w);
+    if(w >= 200) {
+        adsVerEffect();
+    }
+    else {
+        adsHorEffect();
+    }
+    headlineEffect() 
+})
+
+function adsVerEffect() {
+    $("#adscontainer").addClass("adsvercontainer container");
+    $("#adscontainer").css("width", ($(window).width() - $("main").width())/2);
+    $("#adstext").addClass("adsvertext adstext");
+    $("#adstext").css('top', $('#adscontainer').height());
+    $('#adstext').animate({
+        top: '-=' + ($('#adscontainer').height() + $('#adstext').height())
+    }, 30000, function() {
+        adsVerEffect();
+    })
+}
+function adsHorEffect() {
+    $("#adscontainer").addClass("adshorcontainer container");
+    $("#adscontainer").css("left", $("main").position().left);
+    $("#adscontainer").css("width", $("main").width());
+    $("#adstext").addClass("adshortext adstext");
+    $('#adstext').css('left', $("#adscontainer").width()); 
+    $('#adstext').animate({
+        left: '-=' + ($('#adscontainer').width() + $('#adstext').width())
+    }, 30000, function() {
+        adsHorEffect();
+    })
+}
+
+// Lab 4 - 3
+var headlineContent = [
+    { "title":"Bánh flan sữa chua - sự kết hợp hoàn hảo", "photo":"images/trangchu/headline/headline1.jpg" },
+    { "title":"Sữa chua làm từ sữa dê - đậm đà hương vị khó quên", "photo":"images/trangchu/headline/headline2.jpg" }, 
+    { "title":"Thưởng thức sữa chua theo cách của bạn", "photo":"images/trangchu/headline/headline3.jpg" }];
+
+function init() {
+    for(var i=0; i<headlineContent.length; i++) {
+        var item = headlineContent[i];
+        var dc = $("<div>");
+        $("<span><h3>" + item.title + "</h3></span>").appendTo(dc);
+        $("<img src = '" + item.photo + "'/>").appendTo(dc);
+        $("#headline").append(dc);
+    }
+}
+
+function headlineEffect() {
+    init();
+    $("#headline > div:gt(0)").hide();
+    setInterval(function() {
+        $("#headline > div:first")
+        .hide()
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo("#headline");
+    }, 5000);
 }
