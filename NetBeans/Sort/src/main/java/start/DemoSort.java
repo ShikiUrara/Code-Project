12428b6bc01efe3792c04f2dcc19578bc37d4279
Kizuna <time0009000@gmail.com>
2022-03-27 19:50:16 +0700

    change

--- NetBeans/Sort/src/main/java/start/DemoSort.java
+++ NetBeans/Sort/src/main/java/start/DemoSort.java
@@ -309,22 +309,22 @@ public class DemoSort extends javax.swing.JFrame {
             
         }
         if(jRadioSelection.isSelected()) {
-           
+            selectionSort(arrValue);
             printArray(arrValue);
             jLabelCountSwap.setText("Số lần hoán đổi: " + countSwap);
         }
         if(jRadioInsertion.isSelected()) {
-            
+            insertionSort(arrValue);
             printArray(arrValue);
             jLabelCountSwap.setText("Số lần hoán đổi: " + countSwap);
         }
         if(jRadioHeap.isSelected()) {
-            
+            heapSort(arrValue);
             printArray(arrValue);
             jLabelCountSwap.setText("Số lần hoán đổi: " + countSwap);
         }
         if(jRadioQuick.isSelected()) {
-            
+            quickSort(arrValue, 0, arrValue.length-1);
             printArray(arrValue);
             jLabelCountSwap.setText("Số lần hoán đổi: " + countSwap);
         }
@@ -508,12 +508,139 @@ public class DemoSort extends javax.swing.JFrame {
                 if ( (Integer) arr[j] > (Integer) arr[j+1])
                 {
                     // swap arr[j+1] and arr[j]
-                    int temp = ( Integer ) arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+//                    int temp = ( Integer ) arr[j];
+//                    arr[j] = arr[j+1];
+//                    arr[j+1] = temp;
+                    swap(arr, j+1, j);
                     Swap(arrLabel[j], arrLabel[j+1]);
                 }
     }
+    
+    public void selectionSort(int[] arr) {
+        for(int i=0; i<arr.length-1; i++) {
+            int indexMin = i;
+            for(int j=i+1; j<arr.length; j++)
+                if(arr[indexMin]>arr[j])
+                    indexMin = j;
+            swap(arr, i, indexMin);
+            Swap(arrLabel[i], arrLabel[indexMin]);
+        }
+    }
+    public void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                // swap(arr, j+1, j);
+                Swap(arrLabel[j], arrLabel[j+1]);
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public void heapSort(int arr[])
+    {
+        int n = arr.length;
+ 
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+ 
+        // One by one extract an element from heap
+        for (int i = n - 1; i > 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            Swap(arrLabel[0], arrLabel[i]);
+ 
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+ 
+    // To heapify a subtree rooted with node i which is
+    // an index in arr[]. n is size of heap
+    void heapify(int arr[], int n, int i)
+    {
+        int largest = i; // Initialize largest as root
+        int l = 2 * i + 1; // left = 2*i + 1
+        int r = 2 * i + 2; // right = 2*i + 2
+ 
+        // If left child is larger than root
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+ 
+        // If right child is larger than largest so far
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+ 
+        // If largest is not root
+        if (largest != i) {
+            swap(arr, i, largest);
+            Swap(arrLabel[i], arrLabel[largest]);
+            // int swap = arr[i];
+            // arr[i] = arr[largest];
+            // arr[largest] = swap;
+ 
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
+        }
+    }
+
+    static int partition(int[] arr, int low, int high)
+    {
+        // pivot
+        int pivot = arr[high];
+        
+        // Index of smaller element and
+        // indicates the right position
+        // of pivot found so far
+        int i = (low - 1);
+    
+        for(int j = low; j <= high - 1; j++)
+        {
+            
+            // If current element is smaller
+            // than the pivot
+            if (arr[j] < pivot)
+            {
+                
+                // Increment index of
+                // smaller element
+                i++;
+                swap(arr, i, j);
+                Swap(arrLabel[i], arrLabel[j]);
+            }
+        }
+        swap(arr, i + 1, high);
+        Swap(arrLabel[i+1], arrLabel[high]);
+        return (i + 1);
+    }
+    
+    /* 
+            arr[] --> Array to be sorted,
+            low --> Starting index,
+            high --> Ending index
+    */
+    public void quickSort(int[] arr, int low, int high)
+    {
+        if (low < high)
+        {
+            
+            // pi is partitioning index, arr[p]
+            // is now at right place
+            int pi = partition(arr, low, high);
+    
+            // Separately sort elements before
+            // partition and after partition
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+    
     // In mang test
     public void printArray(int arr[])
     {
@@ -523,8 +650,8 @@ public class DemoSort extends javax.swing.JFrame {
         System.out.println();
     }
     
-    // Phuong thuc hoan doi
-    	public void Swap(JLabel lb1, JLabel lb2) {
+    // Phuong thuc hoan doi mang label
+    	public static void Swap(JLabel lb1, JLabel lb2) {
 		int x1 = lb1.getX();
 		int x2 = lb2.getX();
 		countThread ++;
@@ -559,18 +686,39 @@ public class DemoSort extends javax.swing.JFrame {
 			        lb1.setLocation(x1, 160);
 		        	lb2.setLocation(x2, 160);
 		    	} catch (Exception e) {
+                            e.printStackTrace();
 		    	}
 		    }
 		});
 		threads[temp].start();
 	}
         
-        // Dung cac luong
-        public static void stopThread() {
-            for( int i = 0; i <= countThread; i++) {
-                threads[i].interrupt();
-            }
-            countThread = -1;
+        // Phuong thuc hoan doi mang thuc te
+        public static void swap(int[] arr, int i, int j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
         
+        // Dung cac luong
+        public static void stopThread() {
+            try {
+                    for( int i = 0; i <= countThread; i++) {
+                        threads[i].interrupt(); 
+                    }
+                    countThread = -1;
+                }
+            catch(Exception e) {
+                e.getStackTrace();
+            }
+        }
+        
+        // Truong hop chay
+        public void runUse(int use) {
+            switch(use) {
+                case 0:
+                    jMenuRun.setEnabled(false);
+                        
+            }
+        }
 }
